509cb036809890efa40010348813db2da4143493
Youssefanter <yooiialy@gmail.com>
2020-11-16 22:37:52 +0200

    Adding reviews and making components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import data from './data';
+import Product from './components/Product'
+
 function App() {
   return(
     <div className="grid-container">
@@ -16,27 +18,7 @@ function App() {
     <main>
         <div className="row center">
           {data.products.map((products)=>(
-              <div key={products._id} className="card">
-                <a href={`/product/${products._id}`}>
-                    <img className="medium" src={products.image} alt={products.name}>
-                      </img>
-                </a>
-                <div className="card-body">
-                <a href={`/product/${products._id}`}>
-                      <h2>{products.name}</h2>
-                    </a>
-                    <div className="rating">
-                        <span> <i className="fa fa-star"></i> </span>
-                        <span> <i className="fa fa-star"></i> </span>
-                        <span> <i className="fa fa-star"></i> </span>
-                        <span> <i className="fa fa-star"></i> </span>
-                        <span> <i className="fa fa-star"></i> </span>
-                    </div>
-                    <div className="price">
-                        ${products.price}
-                    </div>
-                </div>
-            </div>
+              <Product key={products._id} products={products}></Product>
             ))
           }
             
---
+++ frontend/src/components/Product.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Rating from './Rating';
+
+export default function Product(props) {
+    const {products}=props;
+    return(
+            <div key={products._id} className="card">
+                <a href={`/product/${products._id}`}>
+                    <img className="medium" src={products.image} alt={products.name}>
+                      </img>
+                </a>
+                <div className="card-body">
+                <a href={`/product/${products._id}`}>
+                      <h2>{products.name}</h2>
+                    </a>
+                    <Rating 
+                    rating={products.rating} 
+                    numReviews={products.numReviews}
+                    ></Rating>
+                    <div className="price">
+                        ${products.price}
+                    </div>
+                </div>
+            </div>
+   )
+}
