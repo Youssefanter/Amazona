beef07ae0e5f068fa62c47611b1c609ea9cd2d55
Youssefanter <yooiialy@gmail.com>
2020-11-21 18:47:04 +0200

    Adding API to load data from data.js

--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import Product from '../components/Product';
-import data from '../data';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 
 export default function HomeScreen() {
-    return (
+  const [Loading, setLoading] = useState(false);
+  const [Error, setError] = useState(false);
+  const [products, setProducts] = useState([]); 
+  useEffect(() => {
+    const fetchData=async()=>{
+      try{
+        setLoading(true);
+        const{data}=await axios.get('/api/products');
+        setLoading(false);
+        setProducts(data);
+      }
+      catch(err){
+        setError(err.message);
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+  return (
     <div>
-        <div className="row center">
-          {data.products.map((products)=>(
-              <Product key={products._id} products={products}></Product>
-            )) 
-          }
-        </div>
+      {Loading? (<LoadingBox></LoadingBox>)
+      :
+      Error?(<MessageBox variant="danger">{Error}</MessageBox>)
+      :(
+      <div className="row center">
+        {products.map((products)=>(
+        <Product key={products._id} products={products}></Product>
+      ))}
+      </div>
+      )}
     </div> 
     );
 }
