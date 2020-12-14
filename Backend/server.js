847938cbbb82b580f005b38f5d117731e8a78d42
Youssefanter <yooiialy@gmail.com>
2020-12-15 00:56:41 +0200

    Building the first table of Backend using mongodb

--- Backend/server.js
+++ Backend/server.js
@@ -1,26 +1,36 @@
-import express from 'express';
-import data from './data.js';
+import express from "express";
+import mongoose from "mongoose";
+import data from "./data.js";
+import userRouter from "./routers/userRouter.js";
 
 const app = express();
-
-
-app.get('/api/products/:id',(req,res)=>{
-    const product=data.products.find(x=>x._id===req.params.id);
-    if(product){
-        res.send(product);
-    } else{
-        res.status(404).send({message:'Product not Found'});
-    }
-});
-app.get('/api/products',(req,res)=>{
-    res.send(data.products);
+mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/Amazona", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
 });
 
-
-app.get('/',(req,res)=>{
-    res.send('Server is ready');
+app.get("/api/products/:id", (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product not Found" });
+  }
 });
-const port =process.env.PORT||5000;
-app.listen(port,()=>{
-    console.log(`Serve at http://localhost:${port}`);
+app.get("/api/products", (req, res) => {
+  res.send(data.products);
+});
+
+app.use("/api/users", userRouter);
+app.get("/", (req, res) => {
+  res.send("Server is ready");
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Serve at http://localhost:${port}`);
 });
